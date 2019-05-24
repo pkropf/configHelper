3ac602270dd15e57b8d3e8465cac4e94df399c03
Peter Kropf <pkropf@gmail.com>
2019-05-24 07:58:37 -0700

    updated configHelper to look for .ini file using the location of the originating program

--- findConfig.py
+++ findConfig.py
@@ -1,11 +1,33 @@
 import os
+import inspect
+
+
+_stack = inspect.stack()
+_program_path = _stack[-1].filename
+_program_dir  = os.path.dirname(_program_path)
+
 
 LOCATIONS=[
+    #look in the same directory as the main program
+    _program_dir,
+
+    #look in the etc directory at the same level as the directory of the main program
+    os.path.join(_program_dir, '../etc'),
+
+    #look in the currect working directory
     '.',
+
+    #look in the user's home directory
     os.path.expanduser('~'),
+
+    #look in /usr/local/etc
     '/usr/local/etc',
+
+    #look in /etc
     '/etc',
 ]
+
+print(LOCATIONS)
 
 def findConfig(name, locations=LOCATIONS):
     """look for a configuration file in a series of known locations."""
