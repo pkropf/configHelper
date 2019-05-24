1d7e708d1629cc1fef44cb722859e4e3798b4e5a
Peter Kropf <pkropf@gmail.com>
2019-05-24 08:37:44 -0700

    added a simple setup.py to allow install via pip.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+from distutils.core import setup
+
+
+setup(name='configHelper',
+      version='1.0',
+      description='Simple functions to help find configuration files',
+      author='Peter Kropf',
+      author_email='pkropf@gmail.com',
+      url='https://github.com/pkropf/configHelper',
+      packages=['configHelper',],
+)
